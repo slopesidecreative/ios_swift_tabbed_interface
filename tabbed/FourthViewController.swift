0a94a285fa83aea134a7345a11975bbd68805668
Erik Clineschmidt <eclineschmidt@gmail.com>
2016-11-15 12:42:32 -0800

    init

---
+++ tabbed/FourthViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FourthViewController.swift
+//  tabbed
+//
+//  Created by Erik Clineschmidt on 11/14/16.
+//  Copyright © 2016 Erik Clineschmidt. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("fourth")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tabbed/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tabbed
+//
+//  Created by Erik Clineschmidt on 11/14/16.
+//  Copyright © 2016 Erik Clineschmidt. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
